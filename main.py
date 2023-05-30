f2fda704918a86b8c5f8b283351d58580634efac
Lipemanso <contatofemanso@gmail.com>
2023-05-30 15:18:24 -0300

    Utilizando o recurso Element do módulo python externo

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import datetime as dt
 print("Hello World")
 
 def mostrar_data():
-    Element('data').write(str(dt.date.today()))
-    print(dt.date.today())
+   Element('data').write(str(dt.date.today()))
+   print(dt.date.today())
 
     
 mostrar_data()
+
+
+def write_to_page():
+      manual_div = Element("manual-write")
+      manual_div.element.innerHTML = "<p><b>Hello World</b></p>"
+      
